5ef802755a325109c4a0c643721e8aba9539f1be
Fabio Carpinato <fabiocarpinato@outlook.it>
2024-10-26 15:13:23 +0200

    improves writing to the streams and clients can now kind of chat between each other

--- client.zig
+++ client.zig
@@ -6,14 +6,18 @@ pub fn main() !void {
     const stream = try std.net.tcpConnectToAddress(server_address);
     defer stream.close();
 
-    var inputThread = try std.Thread.spawn(.{}, handleInputThread, .{stream});
-    var outputThread = try std.Thread.spawn(.{}, handleOutputThread, .{stream});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var inputThread = try std.Thread.spawn(.{}, handleInputThread, .{ stream, allocator });
+    var outputThread = try std.Thread.spawn(.{}, handleOutputThread, .{ stream, allocator });
 
     inputThread.join();
     outputThread.join();
 }
 
-fn handleInputThread(stream: std.net.Stream) !void {
+fn handleInputThread(stream: std.net.Stream, _: std.mem.Allocator) !void {
     const stdin = std.io.getStdIn();
 
     var stdin_buffer: [1024]u8 = undefined;
@@ -22,27 +26,21 @@ fn handleInputThread(stream: std.net.Stream) !void {
         const input = try stdin.reader().readUntilDelimiterOrEof(&stdin_buffer, '\n');
 
         if (input) |m| {
-            std.debug.print("message {s}\n", .{m});
-
             var writer = stream.writer();
             _ = try writer.write(m);
-
-            std.debug.print("Message {s} sent to the server\n", .{m});
         }
     }
 }
 
-fn handleOutputThread(stream: std.net.Stream) !void {
+fn handleOutputThread(stream: std.net.Stream, _: std.mem.Allocator) !void {
     var stream_buffer: [1024]u8 = undefined;
 
     while (true) {
         const read_bytes = try stream.reader().read(&stream_buffer);
 
-        std.debug.print("Read {} bytes from server\n", .{read_bytes});
-
         if (read_bytes > 0) {
             const server_message = stream_buffer[0..read_bytes];
-            std.debug.print("Received message from server: {s}\n", .{server_message});
+            std.debug.print("{s}\n", .{server_message});
         }
     }
 }
--- server.zig
+++ server.zig
@@ -16,14 +16,14 @@ pub fn main() !void {
     var connected_clients = std.AutoHashMap(u32, std.net.Server.Connection).init(allocator);
     defer connected_clients.deinit();
 
-    var next_client_idx: u32 = 0;
+    var next_client_idx: u32 = 1;
 
     while (true) {
         const client = try server.accept();
 
         try connected_clients.put(next_client_idx, client);
 
-        var thread = try std.Thread.spawn(.{}, handleClient, .{ client, next_client_idx });
+        var thread = try std.Thread.spawn(.{}, handleClient, .{ client, next_client_idx, &connected_clients, &allocator });
 
         next_client_idx += 1;
 
@@ -31,12 +31,14 @@ pub fn main() !void {
     }
 }
 
-fn handleClient(client: std.net.Server.Connection, client_idx: u32) !void {
+fn handleClient(client: std.net.Server.Connection, client_idx: u32, connected_clients: *std.AutoHashMap(u32, std.net.Server.Connection), allocator: *const std.mem.Allocator) !void {
     defer client.stream.close();
 
     std.debug.print("Connection received from {} with index {}\n", .{ client.address, client_idx });
 
-    _ = try client.stream.writer().print("You have been assigned the index {}", .{client_idx});
+    const welcome_message = try std.fmt.allocPrint(allocator.*, "You have been assigned the index {}", .{client_idx});
+    defer allocator.free(welcome_message);
+    _ = try client.stream.writer().write(welcome_message);
 
     const buffer_size = 1024;
     var buffer: [buffer_size]u8 = undefined;
@@ -51,10 +53,17 @@ fn handleClient(client: std.net.Server.Connection, client_idx: u32) !void {
             const message = buffer[0..read_bytes];
             std.debug.print("Received message from {}: {s}\n", .{ client.address, message });
 
-            _ = client.stream.writer().write("your message has been received from the server") catch |err| {
-                std.debug.print("error writing a response for the client {}\n", .{err});
-                break;
-            };
+            var connected_clients_iterator = connected_clients.iterator();
+
+            while (connected_clients_iterator.next()) |entry| {
+                const m = try std.fmt.allocPrint(allocator.*, "Client {d}: {s}", .{ client_idx, message });
+                defer allocator.free(m);
+
+                _ = entry.value_ptr.stream.writer().write(m) catch |err| {
+                    std.debug.print("error writing a response for the client {}\n", .{err});
+                    break;
+                };
+            }
         }
     }
 }
